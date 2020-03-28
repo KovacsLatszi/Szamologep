3f91cf1d96e8be2a895a37cd619dd2c2ae8dc892
Kovács László <kvcslszl00@gmail.com>
2020-03-28 23:01:18 +0100

    refucktor

--- számológép/Program.cs
+++ számológép/Program.cs
@@ -4,93 +4,115 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace számológép
+namespace szamologep
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double szam1;
-            double szam2;
-            string művelet;
-            bool elég = false;
-            string csekk = ("igen");
-            bool lúp = true;
-            
-
-            while (elég != true)
+            CalculatorApp();
+        }
+      
+        
+        static void ParsolasiHiba(string hibasErtek)
+        {
+            Console.WriteLine($"Parsolási hiba: {hibasErtek}");
+        }
+        static void Szamolo(Double szam1, Double szam2, string muvelet)
+        {
+            Double eredmeny;
+            switch (muvelet)
             {
-                lúp = true;
-
-                Console.Write("Add meg az első számot: ");
-                szam1 = Convert.ToDouble(Console.ReadLine());
-
-                Console.Write("Milyen műveletet szeretnél (+=összeadás -=kivonás /=osztás *=szorzás): ");
-                művelet = Console.ReadLine();
-
-                Console.Write("Add meg a második számot: ");
-                szam2 = Convert.ToDouble(Console.ReadLine());
-
-
-                if (művelet == "+")
-                {
-                    Console.WriteLine("Végeredmény: " + (szam1 + szam2));
-                }
-                else if (művelet == "-")
-                {
-                    Console.WriteLine("Végeredmény: " + (szam1 - szam2));
-                }
-                else if (művelet == "/")
-                {
-                    Console.WriteLine("Végeredmény: " + (szam1 / szam2));
-                }
-                else if (művelet == "*")
-                {
-                    Console.WriteLine("Végeredmény: " + (szam1 * szam2));
-                }
-                else
-                {
-                    Console.WriteLine("Ilyen művelet nem létezik.");
-                }
-
-                while (lúp == true)
-                {
-                    Console.WriteLine("Akarsz egy új számolást végezni? (igen/nem)");
-                    csekk = Console.ReadLine();
-
-                    switch (csekk)
+                case "+":
+                    eredmeny = szam1 + szam2;
+                    break;
+                case "-":
+                    eredmeny = szam1 - szam2;
+                    break;
+                case "/":
+                    if (szam2 == 0)
                     {
-                        case "igen":
-                            lúp = false;
-                            elég = false;
-                            break;
-
-                        case "nem":
-                            lúp = false;
-                            elég = true;
-                            break;
-
-                        default:
-                            Console.WriteLine("Csak igen / nem lehet a válasz");
-                            break;
+                        Console.WriteLine("A nullavál való osztás nem értelmezhető!");
+                        return;
                     }
-                }
-                
-
-
-                
-
-                    
-                }
-                
-
-
-
-
-
+                    eredmeny = szam1 / szam2;
+                    break;
+                case "*":
+                    eredmeny = szam1 * szam2;
+                    break;
+                default:
+                    Console.WriteLine("Ilyen művelet nem létezik.");
+                    return;
             }
-            
-           
+            Console.WriteLine($"Végeredmény: {eredmeny}");
+        }
+        static Double ConsoleDoubleParser(String consoleMessage)
+        {
+            Console.Write(consoleMessage);
+            string szamString = Console.ReadLine();
+            // Parsolás
+            Double szam;
+            if (!Double.TryParse(szamString, out szam))
+            {
+                ParsolasiHiba(szamString);
+                throw new Exception($"Parse failed! {szam}");
+            }
+            return szam;
+        }
+        static bool RetryLoop()
+        {
+            bool loop = true;
+            string csekk = "igen";
+            bool continueRunning = false;
+            while (loop == true)
+            {
+                Console.WriteLine("Akarsz egy új számolást végezni? (igen/nem)");
+                csekk = Console.ReadLine();
+                switch (csekk)
+                {
+                    case "igen":
+                        loop = false;
+                        continueRunning = false;
+                        break;
+                    case "nem":
+                        loop = false;
+                        continueRunning = true;
+                        break;
+                    default:
+                        Console.WriteLine("Csak igen / nem lehet a válasz");
+                        break;
+                }
+            }
+            return continueRunning;
+        }
+        static void CalculatorApp()
+        {
+            bool run = true;
+            while (run == true)
+            {
+                Double szam1;
+                Double szam2;
+                string muvelet;
+                try
+                {
+                    szam1 = ConsoleDoubleParser("Add meg az első számot");
+                    Console.Write("Milyen műveletet szeretnél (+=összeadás -=kivonás /=osztás *=szorzás): ");
+                    muvelet = Console.ReadLine();
+                    szam2 = ConsoleDoubleParser("Add meg a második számot");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("We're fucked!");
+                    Console.ReadLine();
+                    return;
+                }
+                // fo szamolas
+                Szamolo(szam1, szam2, muvelet);
+                run = RetryLoop();
+                
+            }
         }
     }
-
+    
+        
+    }
